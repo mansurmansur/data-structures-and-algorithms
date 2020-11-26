7c73763643aba10641090878fcc47963df44d00b
mansur mansur <mansurmansur@trentu.ca>
2020-11-26 02:44:25 -0500

    made final changes

--- data-strucutures/Linkedlist.cs
+++ data-strucutures/Linkedlist.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
+//---------------------------------------------------------------------------------------------------------------------------------------
 namespace Linkedlist
 { 
     /// <summary>
@@ -9,44 +12,131 @@ namespace Linkedlist
     public class Node
     {
         #region Field
-        private int data { get; set; }
-        private Node next { get; set; }
+        //properties
+        public int data { get; set; }
+        public Node next { get; set; }
         #endregion
 
         #region Constructor
+        //creates an instance of Node class
         public Node(int data, Node next)
         {
             this.data = data;
             this.next = next;
         }
         #endregion
+
+        #region CompareTo Method
+        //compares two node using their data
+        public int CompareTo(object obj)
+        {
+            if (obj != null)
+                return 1;
+
+            Node other = (Node)obj;  //casting
+
+            if (other != null)
+                return other.data - data;
+            else
+                return 1;
+        }
+        #endregion
     }
+    //-----------------------------------------------------------------------------------------------------------------------------------
     /// <summary>
     /// Class name: Linkedlist
     /// description: this class creates a linkedlist and contains methods that can be used to manipulate the linked list.
     /// </summary>
     public class Linkedlist
     {
-        private Node front;
+        #region Field
+        //properties
+        private Node front;  //head of the linked list
+        #endregion
 
         #region Constructor
+        //creates an empty linkedlist 
         public Linkedlist()
         {
-            front = new Node(0, null);      //created an empty linked list
+            front = new Node(0,null);
         }
         #endregion
 
+        #region Insert Method
+        //insert method
+        public void Insert(Node t)
+        {
+            Node temp = front; 
 
-        #region insert method
-        public void insert(Node t)
+            //when the list is empty
+            if (temp.next == null)
+            {
+                temp.next = t;
+            }
+            else //if not empty
+            {
+                while (temp.next != null)
+                    temp = temp.next;
+
+                //insert
+                temp.next = t;
+            }
+        }
+        #endregion
+        #region Delete Method
+        //delete method
+        //delete with key data
+        public Node Delete(int key)
+        {
+            Node temp = front; Node prev = new Node(0, null);
+            //to delete we need to find the node
+            Node t = Search(key);
+
+            while (temp != null && temp.data != t.data)
+            {
+                prev = temp;
+                temp = temp.next;
+            }
+
+            if(temp.next != null)
+            {
+                prev.next = temp.next;
+            }
+            else
+            {
+                prev.next = null;
+            }
+
+            return t;
+        }
+        #endregion
+
+        #region Search Method
+        //find a node with the data
+        public Node Search(int key)
+        {
+            Node temp = front;         //pointer
+
+            while (temp != null && temp.data != key)
+                temp = temp.next;
+
+            return temp;
+        }
+        #endregion
+        #region
+        public void print()
         {
             Node temp = front;
 
-           
+            while(temp.next != null)
+            {
+                temp = temp.next;
+                Console.WriteLine("{0}", temp.data);
+            }
         }
         #endregion
     }
-
+ //---------------------------------------------------------------------------------------------------------------------------------------
     /// <summary>
     /// Class name: Program
     /// Description: This class contains the main method.
@@ -55,7 +145,31 @@ namespace Linkedlist
     {
         public static void Main(string[] args)
         {
+            
             Console.WriteLine("Welcome to linked list data structures.");
+
+            //5 nodes for testing
+            Node a = new Node(2, null);
+            Node b = new Node(1, null);
+            Node c = new Node(5, null);
+            Node d = new Node(4, null);
+            Node e = new Node(7, null);
+
+            //an instance of linkedlist
+            Linkedlist list = new Linkedlist();
+
+            //inserts the 5 nodes
+            list.Insert(a);
+            list.Insert(b);
+            list.Insert(c);
+            list.Insert(d);
+            list.Insert(e);
+
+            //search
+            list.Delete(4);
+
+            //print
+            list.print();
         }
     }
 }
