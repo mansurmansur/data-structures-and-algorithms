0f52c3cc3a28cba80843a1cfb8567351569d8ba8
Mansur <45202319+mansurmansur@users.noreply.github.com>
2022-07-24 18:16:46 +0000

    file for basic use of array in c with memory management

--- data-strucutures/Array/array.c
+++ data-strucutures/Array/array.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+//main method
+int main(){
+    //variables
+    char name[100];
+
+    //initialize the variable
+    strcpy(name,"Mansur said Mansur");
+
+    printf("%s",name);
+}
